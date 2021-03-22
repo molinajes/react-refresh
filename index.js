ee0b900ab5cfbe9d41f01bd033940379d4c5bfb2
danielnikravesh <devcrazier@gmail.com>
2021-03-22 18:01:13 -0400

    Clear input field after sumbit

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-// let itemInput = document.getElementById("itemInput")
-// const submitItem = document.getElementById("submitItem")
-// let itemList = document.getElementById("itemList")
-
-
+//function for binding submit button to event listener 
 submitItem.addEventListener('click', () => {
+  //container for creating list item 
   let item = document.createElement("li")
+  //assign the input value to list value 
   item.textContent = itemInput.value
+  //add item(node) to parent(ul) node 
   itemList.appendChild(item)
+  //clear input 
+  return clearInput()
 });
+
+//function for clearing input 
+let clearInput = () => {
+  itemInput.value = "";
+}
 
 
