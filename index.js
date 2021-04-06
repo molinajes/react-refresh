9b4c9d730e176b2d1f5b625eed7bd27186ca000b
danielnikravesh <devcrazier@gmail.com>
2021-04-06 16:50:00 -0400

    CEnable item to be marked complete

--- index.js
+++ index.js
@@ -11,12 +11,22 @@ submit.addEventListener("click", () => {
   list.push(item)
   console.log(list)
 
+
   return clearInput()
-});
+})
 
 
 let clearInput = () => {
   input.value = "";
+}
+
+// event handler for marking items complete
+let listItem = document.querySelectorAll("li")
+
+for (let i = 0; i < listItem.length; i++) {
+  listItem[i].addEventListener("click", function() {
+    this.classList.toggle("complete");
+  });
 }
 
 
