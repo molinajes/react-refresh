85092bb304378a18fb3035f39d2a5a173534862f
danielnikravesh <devcrazier@gmail.com>
2021-05-04 19:52:20 -0400

    Prevent empty input from submitting

--- index.js
+++ index.js
@@ -2,43 +2,51 @@ let todoList = {
   todos: []
 }
 
-submit.addEventListener("click", () => {
+submit.addEventListener("click", (e) => {
+  
+  //conditional to check if input is empty
+  if(input.value === "" || input.value === null) {
+    console.log("empty input")
+    e.preventDefault()
+  } else {
+    //create label element 
+    let item = document.getElementById("input").value
+    let label = document.createElement("label")
+    label.className = "complete"
 
-  //create checkbox element 
-  let checkbox = document.createElement("input")
-  checkbox.type= "checkbox"
+    //create checkbox element 
+    let checkbox = document.createElement("input")
+    checkbox.type = "checkbox"
 
-  //create label element 
-  let item = document.getElementById("input").value
-  let label = document.createElement("label")
-  label.className = "complete"
+    //create li element
+    let list = document.createElement("li")
 
-  //create li element
-  let list = document.createElement("li")
+    //append text node to label 
+    label.appendChild(document.createTextNode(item))
 
-  //append text node to label 
-  label.appendChild(document.createTextNode(item))
+    //append checkbox and label to li element
+    list.appendChild(checkbox)
+    list.appendChild(label)
 
-  //append checkbox and label to li element
-  list.appendChild(checkbox)
-  list.appendChild(label)
+    //append li to ul
+    itemList.appendChild(list)
 
-  //append li to ul
-  itemList.appendChild(list)  
+    //add new item to array list
+    todoList.todos.push(item)
 
-  //add new item to array list
-  todoList.todos.push(item)
+    console.log(todoList)
 
-  console.log(todoList)
-
-
-  //clear input field
-  clearInput()
+    //clear input field
+    clearInput()
+  } 
 })
 
 let clearInput = () => {
   input.value = "";
 }
+
+
+
 
 
 
